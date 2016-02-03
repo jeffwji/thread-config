2b1fe96a8ca043d544de248f9f80445cd53407a1
Jeff W <jeffwji@yahoo.com>
2016-02-03 16:53:34 +0800

    update ignore list remove unused class

--- src/main/java/com/wang/thread/configure/ThreadConfig.java
+++ src/main/java/com/wang/thread/configure/ThreadConfig.java
@@ -1,22 +1,17 @@
 package com.wang.thread.configure;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import com.wang.thread.ThreadTool;
-
 @Configuration
 @ComponentScan
-@EnableConfigurationProperties({ PropertySourcesPlaceholderConfigurer.class })
-@Import(ThreadTool.class)
+//@EnableConfigurationProperties({ PropertySourcesPlaceholderConfigurer.class })
+//@Import(ThreadTool.class)
 @PropertySource(value = "classpath:config/thread.properties")
 public class ThreadConfig {
 	/**
--- src/test/java/com/htche/thread/test/unit/TestThread.java
+++ src/test/java/com/htche/thread/test/unit/TestThread.java
@@ -7,28 +7,27 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.wang.thread.ThreadTool;
 import com.wang.thread.configure.ThreadConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {ThreadConfig.class})
+@SpringApplicationConfiguration(classes = { ThreadConfig.class })
 public class TestThread {
-	@Autowired  AsyncTaskExecutor asyncTaskExecutor;
+	@Autowired AsyncTaskExecutor asyncTaskExecutor;
+
 	@Test
-	public void test1(){
-		
-		AsyncTaskExecutor executor=ThreadTool.getAsyncTaskExecutor();
-		executor.execute(new Runnable() {
-			
+	public void test1() {
+
+		asyncTaskExecutor.execute(new Runnable() {
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
 				System.out.println("thread 1");
 			}
 		});
-		AsyncTaskExecutor executor1=ThreadTool.getAsyncTaskExecutor();
-		executor1.execute(new Runnable() {
-			
+
+		asyncTaskExecutor.execute(new Runnable() {
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
@@ -36,7 +35,7 @@ public class TestThread {
 			}
 		});
 		System.out.println("sss");
-		
+
 	}
 
 }
