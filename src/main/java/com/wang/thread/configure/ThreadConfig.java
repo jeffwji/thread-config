07161de4be79a42786a6560a267328815b0bae61
Jeff W <jeffwji@yahoo.com>
2016-02-04 18:16:06 +0800

    update pom.xml

--- src/main/java/com/wang/thread/configure/ThreadConfig.java
+++ src/main/java/com/wang/thread/configure/ThreadConfig.java
@@ -1,17 +1,15 @@
 package com.wang.thread.configure;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
-@ComponentScan
-//@EnableConfigurationProperties({ PropertySourcesPlaceholderConfigurer.class })
-//@Import(ThreadTool.class)
+@EnableAutoConfiguration
 @PropertySource(value = "classpath:config/thread.properties")
 public class ThreadConfig {
 	/**
